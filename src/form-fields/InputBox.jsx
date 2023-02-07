78ae76e0ac20dd0035c96c6c5bdb7cb670ced7e0
Prabhat Patel <prabhatpatel508@outlook.com>
2023-02-08 01:09:25 +0530

    form validate successfull

--- src/form-fields/InputBox.jsx
+++ src/form-fields/InputBox.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import './style.css'
-const InputBox = ({label,onChange,...otherProps}) => {
-  return (
-    <div className='inputField'>
-            <label>{label}</label>
-            <input {...otherProps} onChange={onChange}/>
-            <span>Erro Message</span>
-    </div>
-  )
-}
+import React, { useState } from "react";
 
-export default InputBox
+import "./style.css";
+
+const InputBox = ({ label, onChange, errorMessage, ...otherProps }) => {
+    const [focused, setFocused] = useState(false);
+
+    return (
+        <div className="inputField">
+            <label>{label}</label>
+            <input
+                {...otherProps}
+                onChange={onChange}
+                onBlur={() => setFocused(true)}
+                onFocus={() =>
+                    otherProps.name === "confirmPassword" && setFocused(true)
+                }
+                focused={focused.toString()}
+            />
+            <span>{errorMessage}</span>
+        </div>
+    );
+};
+
+export default InputBox;
